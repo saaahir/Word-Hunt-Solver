e59ec6709095df4bb92add809d7a5411331c0af1
saaahir <saahirdhanani@gmail.com>
2025-09-16 09:24:35 -0400

    add placeholder text

--- src/components/SolutionDisplay.tsx
+++ src/components/SolutionDisplay.tsx
@@ -13,19 +13,23 @@ const SolutionDisplay = ({ answers, handleMouseEvent }: SolutionDisplayProps) =>
     ]);
 
     return (
-        // <div className="bg-boardGreen border-borderGreen border-8 rounded-2xl mt-2 flex flex-col items-center overflow-y-auto w-[353px] h-[353px] lg:w-[484px] lg:h-[480px]">
         <div className="bg-boardGreen border-borderGreen border-8 rounded-2xl mt-2 flex flex-col items-center overflow-y-auto w-[353px] h-[353px] lg:w-[484px] lg:h-[480px]">
-
-            {Array.from(answers.entries()).map(([word, pathlist], idx) => (
-                <div
-                    key={word}
-                    onMouseOver={() => handleMouseEvent(pathlist, idx)}
-                    className="bg-[url('/resources/wood.png')] bg-cover bg-center rounded-md my-1 flex flex-row font-bold text-lg border shadow-xl text-center w-[98%] place-content-between hover:opacity-60 text-black"
-                >
-                    <div>{word}</div>
-                    <div>{word.length <= 8 ? pointsMap.get(word.length) : pointsMap.get(8)}</div>
+            {answers.size === 0 ? (
+                <div className="text-white font-bold text-xl mb-2 text-center w-full flex items-center justify-center h-full">
+                    Answers show here!
                 </div>
-            ))}
+            ) : (
+                Array.from(answers.entries()).map(([word, pathlist], idx) => (
+                    <div
+                        key={word}
+                        onMouseOver={() => handleMouseEvent(pathlist, idx)}
+                        className="bg-[url('/resources/wood.png')] bg-cover bg-center rounded-md my-1 flex flex-row font-bold text-lg border shadow-xl text-center w-[98%] place-content-between hover:opacity-60 text-black"
+                    >
+                        <div>{word}</div>
+                        <div>{word.length <= 8 ? pointsMap.get(word.length) : pointsMap.get(8)}</div>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
